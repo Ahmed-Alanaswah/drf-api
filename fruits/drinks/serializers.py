7b0f03e8ea604ec4a2f679af7a0b5dfb1aa72fed
Ahmed-Alanaswah <alanaswehahmed0@gmail.com>
2021-12-12 21:58:41 +0200

    lab13

---
+++ fruits/drinks/serializers.py
@@ -0,0 +1,9 @@
+from django.db.models import fields
+from rest_framework import serializers
+
+from .models import Drink 
+
+class DrinkSerializers(serializers.ModelSerializer):
+    class Meta :
+        fields=['title','author','updated','timestamp','content']
+        model=Drink
